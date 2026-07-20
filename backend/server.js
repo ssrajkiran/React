77e0f9b070917059ea1b6fb6766a19a1b499cabf
RAJKIRAN S S <67040734+ssrajkiran@users.noreply.github.com>
2026-07-20 17:25:58 +0530

    Fix require paths in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",require("./routes/aut"));
+app.use("/api/auth",require("./routes/auth"));
 
 app.use("/api/leaves",require("./routes/leaves"));
 
